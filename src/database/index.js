90b0563e9bd7f2fbb77f1f9107389eec40b49817
jnware7 <jnware7@gmail.com>
2017-07-17 16:15:23 -0700

    added bcrypt

--- src/database/index.js
+++ src/database/index.js
@@ -1,11 +1,16 @@
 const pgp = require('pg-promise')();
 const connectionString = 'postgres://localhost:5432/roam';
 const db = pgp(connectionString);
+const bcrypt = require('bcrypt')
+const saltRounds = 12;
 
 const CREATE_USER = `INSERT INTO users (username, password) VALUES($1, $2) RETURNING *`
 
 const createUser = (username,password) => {
-  return db.one(CREATE_USER, [username, password])
+  return bcrypt.hash(password, saltRounds)
+  .then(function(hash) {
+    return db.one(CREATE_USER, [username, hash])
+  });
 };
 
 
