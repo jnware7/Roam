4b69ad95ce28a697b192327b193fb51123eeeb15
jason00111 <emberleyjason@gmail.com>
2017-07-18 17:34:26 -0700

    cleans up code

--- src/auth.js
+++ src/auth.js
@@ -6,21 +6,17 @@ const LocalStrategy = require('passport-local').Strategy
 passport.use(new LocalStrategy(
   function(username, password, done) {
     findByUsername(username)
-    .then(result =>{
-      const user = result[0]
+    .then(user =>{
       if (!user) {
         return done(null, false, { message: 'Incorrect username.' })
       }
       bcrypt.compare(password, user.password, (error, result) => {
         if (error) {
-          console.log('entered LC 1');
           return done(error)
         }
         if (!result) {
-          console.log('entered LC 2');
           return done(null, false)
         }
-        console.log('entered LC 3', user);
         return done(null, user)
       })
     })
@@ -28,17 +24,14 @@ passport.use(new LocalStrategy(
 )
 
 passport.serializeUser(function(user, done){
-  console.log('in serializeUser.  user is ->', user)
   done(null, user.username)
 })
 
 passport.deserializeUser(function(username,done){
-  console.log('in deserializeUser')
   findByUsername(username)
-  .then(user =>{
-    console.log('in deserializeUser.  user is ->', user)
-    done(null, user)
-  })
+    .then(user =>{
+      done(null, user)
+    })
 })
 
 module.exports = { passport }
--- src/server.js
+++ src/server.js
@@ -1,24 +1,24 @@
 const express = require('express');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const bodyParser = require('body-parser');
+const hbs = require('hbs');
+
+const { passport } = require('./auth');
+const { createUser, findByUsername, getUserById,
+  getReviewsByUserId, getCurrentCityByUserId } = require('./database');
+
 const app = express();
+const router = express.Router();
 const port = process.env.PORT || 3000;
-const hbs = require('hbs')
-const {createUser, findByUsername, getUserById , getReviewsByUserId,  getCurrentCityByUserId} = require('./database')
-const bodyParser = require('body-parser')
-const {passport} = require('./auth')
-const cookieParser = require('cookie-parser')
-const cookieSession = require('cookie-session')
-const session = require('express-session')
 
-app.use(bodyParser.urlencoded({extended: false}))
-app.use(session({
-  secret: 'keyboard cat'
-}))
-app.use(cookieParser())
-app.use(passport.initialize())
-app.use(passport.session())
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(session({ secret: 'keyboard cat' }));
+app.use(cookieParser());
+app.use(passport.initialize());
+app.use(passport.session());
 
-
-hbs.registerPartial('nav',`<header>
+hbs.registerPartial('nav', `<header>
   <div class=".col-xs-12 .col-md-8"></div>
   <div class=".col-xs-6 .col-md-4">
     <nav>
@@ -29,8 +29,9 @@ hbs.registerPartial('nav',`<header>
       </ul>
     </nav>
   </div>
-</header>`)
-hbs.registerPartial('loggedinnav',`<header>
+</header>`);
+
+hbs.registerPartial('loggedinnav', `<header>
   <div class=".col-xs-12 .col-md-8"></div>
   <div class=".col-xs-6 .col-md-4">
     <nav>
@@ -40,16 +41,14 @@ hbs.registerPartial('loggedinnav',`<header>
       </ul>
     </nav>
   </div>
-</header>`)
+</header>`);
 
 app.use(express.static('src/public'));
 app.set('views', './src/views');
-
 app.set('view engine', 'hbs');
-const router = express.Router();
+
 
 router.get('/', (req, res) => {
-  console.log('in /, session is ->', req.user, req.session, req.passport)
   res.render('splash');
 });
 
@@ -64,12 +63,11 @@ router.get('/login', (req, res) => {
 router.post('/signup', (req, res) => {
   const username = req.body.username;
   const password = req.body.password;
-  const userid = req.params.userid;
 
   createUser(username, password)
-  .then(user=>{
-    res.redirect('/profile')
-  });
+    .then(user=>{
+      res.redirect('/profile')
+    });
 });
 
 router.post('/login', passport.authenticate('local', {
@@ -78,7 +76,6 @@ router.post('/login', passport.authenticate('local', {
 }))
 
 router.use((req, res, next) => {
-  console.log('---> req.user:', req.user)
   if(req.user) {
     next()
   } else {
@@ -91,11 +88,9 @@ router.get('/logout', (req,res) => {
   res.redirect('/')
 })
 
-
-
 router.get('/profile', (req, res) => {
-  console.log('req.user',req.user)
     const userid = req.user.id
+
     Promise.all([
       getUserById(userid),
       getReviewsByUserId(userid),
@@ -105,19 +100,16 @@ router.get('/profile', (req, res) => {
       const user = results[0]
       const reviews = results[1]
       const city = results[2]
-      console.log('reviews ==>',reviews)
+
       res.render('profile', {
         user: user,
         reviews: reviews,
         city: city
       })
     }).catch( err => {
-      console.log("error", err)
+      console.error(err)
     })
 })
-
-
-
 
 app.use('/', router);
 
